00aa1f86d4fdaef271c59a11bbe5be864aeda34f
Atala <alois.guillope@gmail.com>
2018-03-15 11:28:27 +0100

    feat: add some tests

--- tests/AppBundle/Entity/TaskCollectionTest.php
+++ tests/AppBundle/Entity/TaskCollectionTest.php
@@ -23,11 +23,32 @@ abstract class TaskCollectionTest extends TestCase
     public function testAddTaskWithoutPosition()
     {
         $task = new Task();
+        $task2 = new Task();
 
         $this->taskCollection->addTask($task);
 
         $this->assertAddTask($task);
-        $this->assertEquals(-1, $this->taskCollection->getItems()->get(0)->getPosition());
+        $this->assertEquals(0, $this->taskCollection->getItems()->get(0)->getPosition());
+
+        $this->taskCollection->addTask($task);
+
+        $this->assertAddTask($task2);
+        $this->assertEquals(1, $this->taskCollection->getItems()->get(0)->getPosition());
+    }
+
+    public function testAddSameTaskTwiceWithoutPosition()
+    {
+        $task = new Task();
+
+        $this->taskCollection->addTask($task);
+
+        $this->assertAddTask($task);
+        $this->assertEquals(0, $this->taskCollection->getItems()->get(0)->getPosition());
+
+        $this->taskCollection->addTask($task);
+
+        $this->assertAddTask($task);
+        $this->assertEquals(0, $this->taskCollection->getItems()->get(0)->getPosition());
     }
 
     public function testAddTaskWithPosition()
@@ -68,5 +89,126 @@ abstract class TaskCollectionTest extends TestCase
         $this->taskCollection->removeTask($task1);
 
         $this->assertFalse($this->taskCollection->containsTask($task1));
+    }
+
+    public function testUpdateTasksInsertTask()
+    {
+        $task = new Task();
+        $task->setId(1);
+        $task1 = new Task();
+        $task1->setId(2);
+        $newTask = new Task();
+        $newTask->setId(3);
+
+        $this->taskCollection->addTask($task, 0);
+        $this->taskCollection->addTask($task1, 1);
+
+        $tasksToAssign = new \SplObjectStorage();
+        $tasksToAssign[$task] = 0;
+        $tasksToAssign[$newTask] = 1;
+        $tasksToAssign[$task1] = 2;
+        $this->taskCollection->updateTasks($tasksToAssign);
+
+        $this->assertEquals($this->taskCollection->findAt(0)->getTask()->getId(), 1);
+        $this->assertEquals($this->taskCollection->findAt(1)->getTask()->getId(), 3);
+        $this->assertEquals($this->taskCollection->findAt(2)->getTask()->getId(), 2);
+    }
+
+    public function testUpdateTasksPushTask()
+    {
+        $task = new Task();
+        $task->setId(1);
+        $task1 = new Task();
+        $task1->setId(2);
+        $newTask = new Task();
+        $newTask->setId(3);
+
+        $this->taskCollection->addTask($task, 0);
+        $this->taskCollection->addTask($task1, 1);
+
+        $tasksToAssign = new \SplObjectStorage();
+        $tasksToAssign[$task] = 0;
+        $tasksToAssign[$task1] = 1;
+        $tasksToAssign[$newTask] = 2;
+        $this->taskCollection->updateTasks($tasksToAssign);
+
+        $this->assertEquals($this->taskCollection->findAt(0)->getTask()->getId(), 1);
+        $this->assertEquals($this->taskCollection->findAt(1)->getTask()->getId(), 2);
+        $this->assertEquals($this->taskCollection->findAt(2)->getTask()->getId(), 3);
+    }
+
+    public function testUpdateTasksRemoveTask()
+    {
+
+        $task = new Task();
+        $task->setId(1);
+        $task1 = new Task();
+        $task1->setId(2);
+        $toRemove = new Task();
+        $toRemove->setId(3);
+
+        $this->taskCollection->addTask($task, 0);
+        $this->taskCollection->addTask($task1, 1);
+        $this->taskCollection->addTask($toRemove, 2);
+
+        $tasksToAssign = new \SplObjectStorage();
+        $tasksToAssign[$task] = 0;
+        $tasksToAssign[$task1] = 1;
+        $this->taskCollection->updateTasks($tasksToAssign);
+
+        $this->assertEquals($this->taskCollection->findAt(0)->getTask()->getId(), 1);
+        $this->assertEquals($this->taskCollection->findAt(1)->getTask()->getId(), 2);
+        $this->assertEquals($this->taskCollection->findAt(2), null);
+
+    }
+
+    public function testUpdateTasksRemoveTaskNotLast()
+    {
+        $task = new Task();
+        $task->setId(1);
+        $task1 = new Task();
+        $task1->setId(2);
+        $toRemove = new Task();
+        $toRemove->setId(3);
+
+        $this->taskCollection->addTask($task, 0);
+        $this->taskCollection->addTask($toRemove, 1);
+        $this->taskCollection->addTask($task1, 2);
+
+        $tasksToAssign = new \SplObjectStorage();
+        $tasksToAssign[$task] = 0;
+        $tasksToAssign[$task1] = 1;
+        $this->taskCollection->updateTasks($tasksToAssign);
+
+        $this->assertEquals($this->taskCollection->findAt(0)->getTask()->getId(), 1);
+        $this->assertEquals($this->taskCollection->findAt(1)->getTask()->getId(), 2);
+        $this->assertEquals($this->taskCollection->findAt(2), null);
+
+    }
+
+    public function testUpdateTasksReorderTasks()
+    {
+
+        $task = new Task();
+        $task->setId(1);
+        $task1 = new Task();
+        $task1->setId(2);
+        $task2 = new Task();
+        $task2->setId(3);
+
+        $this->taskCollection->addTask($task, 0);
+        $this->taskCollection->addTask($task1, 1);
+        $this->taskCollection->addTask($task2, 2);
+
+        $tasksToAssign = new \SplObjectStorage();
+        $tasksToAssign[$task] = 0;
+        $tasksToAssign[$task1] = 2;
+        $tasksToAssign[$task2] = 1;
+        $this->taskCollection->updateTasks($tasksToAssign);
+
+        $this->assertEquals($this->taskCollection->findAt(0)->getTask()->getId(), 1);
+        $this->assertEquals($this->taskCollection->findAt(1)->getTask()->getId(), 3);
+        $this->assertEquals($this->taskCollection->findAt(2)->getTask()->getId(), 2);
+
     }
 }
